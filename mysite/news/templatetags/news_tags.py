cff33b475313c2c687fa401e40e40c7f658241dc
ArsenAjiev <selftcoder@gmail.com>
2021-09-08 16:03:00 +0300

    add login les-47

--- mysite/news/templatetags/news_tags.py
+++ mysite/news/templatetags/news_tags.py
@@ -13,6 +13,7 @@ def get_categories():
 
 @register.inclusion_tag('news/list_categories.html')
 def show_categories(arg1='Hello', arg2='world'):
-    categories = Category.objects.annotate(cnt=Count('news')).filter(cnt__gt=0)
+    # categories = Category.objects.annotate(cnt=Count('news')).filter(cnt__gt=0)
+    categories = Category.objects.annotate(cnt=Count('news', filter=F('news__is_published'))).filter(cnt__gt=0)
     # categories = Category.objects.all()
     return {"categories": categories, "arg1": arg1, "arg2": arg2}
--- mysite/news/views.py
+++ mysite/news/views.py
@@ -2,10 +2,76 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import ListView, DetailView, CreateView
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.paginator import Paginator
 
 from news.models import News, Category
-from .forms import NewsForm
+from .forms import NewsForm, UserRegisterForm, UserLoginForm
 from .utils import MyMixin
+# from django.contrib.auth.forms import UserCreationForm - delite
+from django.contrib import messages
+from django.contrib.auth import login, logout
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Вы успешно зарегистрировались")
+            return redirect('home')
+        else:
+            messages.error(request, "Ошибка регистрации")
+    else:
+        form = UserRegisterForm()
+    return render(request, 'news/register.html', {"form": form})
+
+
+
+
+
+# def register(request):
+#     if request.method == 'POST':
+#         form = UserCreationForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             messages.success(request, "Вы успешно зарегистрировались")
+#             return redirect('login')
+#         else:
+#             messages.error(request, "Ошибка регистрации")
+#     else:
+#         form = UserCreationForm()
+#     return render(request, 'news/register.html', {"form": form})
+
+def user_login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserLoginForm()
+    return render(request, 'news/login.html', {'form': form})
+
+
+
+def user_logout(request):
+    logout(request)
+    return redirect('login')
+
+
+
+
+def test(request):
+    objects = ['john1', 'paul2', 'george3', 'ringo4', 'john5', 'paul6', 'george7']
+    paginator = Paginator(objects, 2)
+    page_num = request.GET.get('page', 1)
+    page_objects = paginator. get_page(page_num)
+    return render(request, 'news/test.html', {'page_obj': page_objects})
+
+
+
+
 
 
 class HomeNews(MyMixin, ListView):
@@ -16,6 +82,7 @@ class HomeNews(MyMixin, ListView):
         'title': 'Главная'
     }
     mixin_prop = 'Hello world'
+    paginate_by = 3
 
 
 
@@ -36,6 +103,7 @@ class NewsByCategory( ListView):
     template_name = 'news/home_news_list.html'
     context_object_name = 'news'
     allow_empty = False
+    paginate_by = 3
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
