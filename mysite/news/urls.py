222d61ef10770c12e7954099bfe2187c38cff057
ArsenAjiev <selftcoder@gmail.com>
2021-09-10 14:01:18 +0300

    finish part 1

--- mysite/news/urls.py
+++ mysite/news/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 from .views import *
 
 urlpatterns = [
     path('register/', register, name='register'),
     path('login/', user_login, name='login'),
     path('logout/', user_logout, name='logout'),
-    path('test/', test, name='test'),
+    path('contact/', contact, name='contact'),
     path('', HomeNews.as_view(), name='home'),
+    # path('', cache_page(60)(HomeNews.as_view()), name='home'),
     #  path('', index, name='home'),
     #  path('category/<int:category_id>/', get_category, name='category'),
     path('category/<int:category_id>/', NewsByCategory.as_view(), name='category'),
--- mysite/news/views.py
+++ mysite/news/views.py
@@ -5,11 +5,12 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.paginator import Paginator
 
 from news.models import News, Category
-from .forms import NewsForm, UserRegisterForm, UserLoginForm
+from .forms import NewsForm, UserRegisterForm, UserLoginForm, ContactForm
 from .utils import MyMixin
 # from django.contrib.auth.forms import UserCreationForm - delite
 from django.contrib import messages
 from django.contrib.auth import login, logout
+from django.core.mail import send_mail
 
 def register(request):
     if request.method == 'POST':
@@ -59,15 +60,33 @@ def user_logout(request):
     logout(request)
     return redirect('login')
 
+def contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            mail = send_mail(form.cleaned_data['subject'], form.cleaned_data['content'], 'aax76@mail.ru', ['aax83@mail.ru'], fail_silently=False)
+            if mail:
+                messages.success(request, 'Письмо отправлено')
+                return redirect('contact')
+            else:
+                messages.error(request, 'Ошибка отправки')
+
+        else:
+            messages.error(request, "Ошибка валидации")
+    else:
+        form = ContactForm()
+    return render(request, 'news/test.html', {"form": form})
 
 
 
-def test(request):
-    objects = ['john1', 'paul2', 'george3', 'ringo4', 'john5', 'paul6', 'george7']
-    paginator = Paginator(objects, 2)
-    page_num = request.GET.get('page', 1)
-    page_objects = paginator. get_page(page_num)
-    return render(request, 'news/test.html', {'page_obj': page_objects})
+
+
+# def test(request):
+#     objects = ['john1', 'paul2', 'george3', 'ringo4', 'john5', 'paul6', 'george7']
+#     paginator = Paginator(objects, 2)
+#     page_num = request.GET.get('page', 1)
+#     page_objects = paginator. get_page(page_num)
+#     return render(request, 'news/test.html', {'page_obj': page_objects})
 
 
 
